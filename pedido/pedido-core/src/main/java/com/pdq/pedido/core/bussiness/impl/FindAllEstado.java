3860cb07a3261873690c5b9ce0fc487787dbe339
Wesley Silva <wesley.silva@muralis.com.br>
2019-01-14 14:49:55 -0200

    Update remove IJPADAO add GenericDAO à consulta de estado.

--- pedido/pedido-core/src/main/java/com/pdq/pedido/core/bussiness/impl/FindAllEstado.java
+++ pedido/pedido-core/src/main/java/com/pdq/pedido/core/bussiness/impl/FindAllEstado.java
@@ -1,25 +1,27 @@
 package com.pdq.pedido.core.bussiness.impl;
 
-import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.dvsmedeiros.bce.core.controller.INavigationCase;
 import com.dvsmedeiros.bce.core.controller.business.IStrategy;
-import com.dvsmedeiros.bce.core.dao.IJPADAO;
+import com.dvsmedeiros.bce.core.dao.impl.GenericDAO;
+import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 import com.pdq.pedido.domain.Estado;
 
 @Component
 public class FindAllEstado implements IStrategy<Estado> {
 	
-	@Autowired private IJPADAO<Estado> dao;
+	@Autowired private GenericDAO<Estado> dao;
 	
 	@Override
 	public void process(Estado aEntity, INavigationCase<Estado> aCase) {
-		List<Estado> listEstado = dao.findAll();
+		Optional<Stream<? extends DomainSpecificEntity>> listEstado = dao.findAll(Estado.class, true);
 		
-		if(listEstado != null) {
+		if(!listEstado.isPresent()) {
 			aCase.suspendExecution("Erro ao realizar a consulta de " + Estado.class.getSimpleName());
 			return;
 		}
